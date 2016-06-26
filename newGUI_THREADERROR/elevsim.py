6c44bc3d3dae8ae950812bf9bdb7858c563cbdbb
vantovan <vantovan7414@gmail.com>
2016-06-26 21:14:52 +0900

    newGUI : THREADERROR : 스레드 다 멈추면 멈추는거 해결해야됨

---
+++ newGUI_THREADERROR/elevsim.py
@@ -0,0 +1,298 @@
+# -*- coding: utf-8 -*-
+
+
+
+# import elevsupp
+# import display
+# import sys
+# sys.path.append("C:\Users\DS\PycharmProjects\untitled2\elevator_algorithm")
+# import elevsim
+
+import ThreadEli2
+import call as c
+import constant
+import dataParsing as gdata
+import elevator
+import readcalls
+import simulator
+
+
+class Elevsim(object):
+    def __init__(self, dialog):
+        print("__elevsim__ \n")
+        self.current_floor_term = 20
+        # self.ui = simulator.ui.getUI()
+        #self.ui = ui  # simulator.Ui_MainWindow()
+        self.dialog = dialog # 다이얼로그 받아온다
+        self.index = 0
+        self.tn = 0 # 가지고 올 데이터의 우치
+        self.elevator_rack = []  # 엘레베이터 호기
+
+        self.elevator_calls = []  # 엘리베이터 콜들
+        self.last_floor = []  # 마지막 층 : 최근층
+
+        self.floor_passengers = []  # 엘베 승객수
+
+        for index in range(0, constant.MAX_ELEVATORS):  # 메인에서 max elevators - 4 정의
+            self.elevator_rack.append(None)  # null값 넣어야 됨, elevator_rack -
+            self.last_floor.append(constant.TOP_FLOOR)
+
+        for index in range(0, constant.MAX_FLOORS):
+            self.floor_passengers.append(0)
+
+            # initialize at something other than zero - 0 말고 다른 것으로 초기화 하시오
+            self.last_max_flow_rate = 1  # 일단 1으로 넣어놓음 - 어차피 나중에 max_flow_rate로 치환된다.
+            self.last_min_flow_rate = 1
+
+            # initialize rtfgjtt6\]
+            # the rest
+            self.min_flow_rate = 0
+            self.max_flow_rate = 0
+            self.total_passengers = 0
+            self.min_duration = 0
+            self.max_duration = 0
+
+    # 이건 시뮬에서 불러진다. - 엘베심에서 init다음으로 제일 먼저 실행됨
+    def run(self,data, ui):
+        #self.ui.toggle()
+        self.ui = ui
+        # 1. 엘레베이터 먼저 생성하겠음
+        for index in range(0, constant.MAX_ELEVATORS):
+            # print(self.elevator_rack[index])
+            if (index < constant.MAX_ELEVATORS):  # 엘베 갯수 받은 것 보다 작은 인덱스만큼 돌려준다.
+                self.elevator_rack[index] = elevator.Elevator(index + 1)  # 1번 엘베 2번엘베 3번엘베 4번엘베
+
+            else:
+                self.elevator_rack[index] = None  # 나머지는 걍 null
+
+        # 2. 처음에 가지고 온 시간을 콜 할당. 입력한 시간에 해당하는 투플 숫자를 가지고 온다
+        self.tn = data #data.generateData(self.ui)
+        registertime = gdata.getData(self.tn)
+        # 해당 투플 (= 콜) 생성시 들어가는 데이터를 읽어온다 - 보기 좋으라고 이렇게 해놈
+        departure_floor = gdata.dataSet[self.tn][2]
+        destination_floor = gdata.dataSet[self.tn][3]
+        # iserroroccured = gdata.dataSet[tn][13]
+        is_up = gdata.dataSet[self.tn][4]
+        passenger = 3  # 일단은 세명이라고 했음
+
+        # 데이터 추가
+        dbn_register_time = gdata.dataSet[self.tn][1] # 수정
+        day_of_week = gdata.dataSet[self.tn][5]
+        iserror = gdata.dataSet[self.tn][6]
+        weekend = gdata.dataSet[self.tn][7] # 수정
+        register15min = gdata.dataSet[self.tn][8]
+
+        # call생성
+        # registertime,departurefloor,destinationfloor,el_idatfield,waitingtime,ridingtime,servicetime,6
+        # registertime_year,registertime_quarter,registertime_month,registertime_day,registertime_hour,registertime_dayofweek, 12
+        # iserroroccured,isup,registertime_30min,registertime_15min,registertime_5min 17
+
+        # register_time, departure_floor,destination_floor, isup, passenger, dbn_register_time, day_of_week, iserror, weekend, register15min):
+        first_call = c.Call(registertime, departure_floor, destination_floor, is_up, passenger, dbn_register_time,day_of_week,iserror,weekend, register15min)  # 1이 up, 0이 down
+
+        # 나중에 웨이팅 구할때 사용 배열에 담는다
+        self.elevator_calls.append(first_call)
+        # 어느 엘리베이터에 할당할지를 고른다
+
+        allocated_elevator = int(self.allocate2(first_call))
+        # 할당 될 엘리베이터에 첫 콜을 레디리스트에 추가해 준다.
+        self.elevator_rack[allocated_elevator].ready_calls.append(first_call)
+
+        # 3. 시간이 같을 때마다 콜을 발생시켜야 된다.
+
+        # 가정 : 시간순으로 데이터가 정렬되어있다, 시간이 지나기 전에검색 할 수 있다.
+        # 해당 투플 다음 한줄씩 읽는다
+        # 스레드 실행
+
+
+
+        t1 = ThreadEli2.ElavatorThread1()
+        t1.setUI(self.ui)
+        t1.setDasom(self.tn)
+        t1.setDialog(self.dialog)
+        t1.setElevator_rack(self.elevator_rack[0])
+
+        t2 = ThreadEli2.ElavatorThread2()
+        t2.setUI(self.ui)
+        t2.setDasom(self.tn)
+        t2.setDialog(self.dialog)
+        t2.setElevator_rack(self.elevator_rack[1])
+
+        t3 = ThreadEli2.ElavatorThread3()
+        t3.setUI(self.ui)
+        t3.setDasom(self.tn)
+        t3.setDialog(self.dialog)
+        t3.setElevator_rack(self.elevator_rack[2])
+
+        t4 = ThreadEli2.ElavatorThread4()
+        t4.setUI(self.ui)
+        t4.setDasom(self.tn)
+        t4.setDialog(self.dialog)
+        t4.setElevator_rack(self.elevator_rack[3])
+
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+
+        readcall = readcalls.Readcalls()
+        readcall.setDaemon(True)
+        readcall.setUI(self.ui)
+        readcall.setDialog(self.dialog)
+        readcall.settn(self.tn)
+        readcall.setcalllist(self.elevator_calls)
+        readcall.setElevator_rack(self.elevator_rack,t1,t2,t3,t4)
+        readcall.start()
+
+
+
+
+
+
+
+
+
+
+
+        # #eleThread1 = ElavatorThread1()
+        # eleThread1.start()
+
+    def getcall(self, tn):  # ui객체를 받아와야 된다
+        # index = 0
+        # 데이터의 시간을 가지고와서 배열에 넣어 놓는다,- 아무튼 시간을 가지고 온다는 뜻
+
+        # tn = 투플넘버를가지고 옵시다 - dataparsing에 접근
+        registertime = gdata.getData(tn)
+        # 해당 투플 (= 콜) 생성시 들어가는 데이터를 읽어온다 - 보기 좋으라고 이렇게 해놈
+        departure_floor = gdata.dataSet[tn][2]
+        destination_floor = gdata.dataSet[tn][3]
+        # iserroroccured = gdata.dataSet[tn][13]
+        is_up = gdata.dataSet[tn][4]
+        passenger = 3  # 일단은 세명이라고 했음
+
+        # 데이터 추가
+        dbn_register_time = gdata.dataSet[tn][1] # 수정
+        day_of_week = gdata.dataSet[tn][5]
+        iserror = gdata.dataSet[tn][6]
+        weekend = gdata.dataSet[tn][7] # 수정
+        register15min = gdata.dataSet[tn][8]
+
+        # call생성
+        # registertime,departurefloor,destinationfloor,el_idatfield,waitingtime,ridingtime,servicetime,6
+        # registertime_year,registertime_quarter,registertime_month,registertime_day,registertime_hour,registertime_dayofweek, 12
+        # iserroroccured,isup,registertime_30min,registertime_15min,registertime_5min 17
+
+        # register_time, departure_floor,destination_floor, isup, passenger, dbn_register_time, day_of_week, iserror, weekend, register15min):
+        new_call = c.Call(registertime, departure_floor, destination_floor, is_up, passenger, dbn_register_time,day_of_week,iserror,weekend, register15min)  # 1이 up, 0이 down
+        #new_call = c.Call(registertime, departure_floor, destination_floor, is_up, passenger)  # 1이 up, 0이 down
+        self.elevator_calls.append(new_call)
+        print('departure_floor : ',new_call.departure )
+        self.elevator_calls.append(new_call)  # 나중에 waitingtime 계산할때 쓸라고 배열에 담아놓는다.
+
+
+        return new_call
+
+
+    # allocate for one by one
+    def allocate2(self, call):
+        # 엘레베이터들의 위치를 가진다
+
+        close_ele = 0  # 가장 가까운 엘레베이터 번호
+          # 층 차이값일단 최대로 줌
+
+        # 각각의 엘리베이터들마다 상황을 비교한다
+
+        for index in range(0, constant.MAX_ELEVATORS):
+            if (int(self.elevator_rack[index].floor) == call.departure):
+                print('check1   :', close_ele)
+                close_ele = index
+                # self.elevator_rack[index].state = constant.LOAD_STATE  # 이거 다시 해야됨 아무튼 태운다는 뜻입니다
+            # 엘베 위치가 나의 출발층보다 아래이고  call이 위로 올라가는 콜이면 할당리스트에 넣겠다.
+            elif (int(self.elevator_rack[index].floor) < call.departure and int(call.isup) == 1):  # 1이 up, 0이 down
+                # 층 차이
+                print('check2   :', close_ele)
+                floor_term = int(call.departure) - int(self.elevator_rack[index].floor)
+                if (floor_term < self.current_floor_term):
+                    self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                    close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+            # 내 콜이 내려가는콜이고 , 내위치보다 엘레베이터가 위에 있을 때
+            elif (call.departure < int(self.elevator_rack[index].floor) and int(call.isup) == 0):
+                # 층차이
+                print('check3   :', close_ele)
+                floor_term = int(self.elevator_rack[index].floor) - int(call.departure)
+                if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                    self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                    close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+
+        print('closest elevator  :', close_ele)
+        return close_ele
+
+
+
+
+
+
+    def allocate(self, call):
+        close_ele = 0  # 가장 가까운 엘레베이터 번호
+
+        for index in range(0, constant.MAX_ELEVATORS):
+            if (self.elevator_rack[index].floor == call.departure):
+                close_ele = index
+                # self.elevator_rack[index].state = constant.LOAD_STATE  # 이거 다시 해야됨 아무튼 태운다는 뜻입니다
+            # 엘베 위치가 나의 출발층보다 아래이고  call이 위로 올라가는 콜이면 할당리스트에 넣겠다.
+            elif (self.elevator_rack[index].floor < call.departure and call == 1):  # 1이 up, 0이 down
+                # 층 차이
+
+                floor_term = call.departure - self.elevator_rack[index].floor
+                if (floor_term < self.current_floor_term):
+                    current_floor_term = floor_term  # 층 차이를 업데이트한다
+                    close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+            # 내 콜이 내려가는콜이고 , 내위치보다 엘레베이터가 위에 있을 때
+            elif (call.departure < self.elevator_rack[index].floor and call == 0):
+                # 층차이
+                floor_term = self.elevator_rack[index].floor - call.departure
+                if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                    current_floor_term = floor_term  # 층 차이를 업데이트한다
+                    close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+
+            print('closest elevator  :', close_ele)
+            return close_ele
+
+
+
+
+
+
+
+
+
+
+
+            # 각 엘베처럼
+            # 내가 올라가는 방향의 콜일 때.
+            # 엘베위치가 해당 콜의 위치보다 아래라면 , 그 엘베를 할당 후보군(리스트)에 포함시킨다.
+            # 내가 내려가는 방향의 콜일 때.
+            # 엘베위치가 해당 콜의 위치보다 위라면 , 그 엘베를 할당 후보군(리스트)에 포함시킨다.
+
+            # 콜의 위치보다 아래라면
+
+    """
+    def run(self):
+        #1.  엘레베이터를 생성한다 - 4
+        for index in range(0, constant.MAX_ELEVATORS):
+            # print(self.elevator_rack[index])
+            if(index<constant.MAX_ELEVATORS): # 엘베 갯수 받은 것 보다 작은 인덱스만큼 돌려준다.
+                self.elevator_rack[index] = elevator.Elevator(index+1) # 1번 엘베 2번엘베 3번엘베 4번엘베
+            else:
+                self.elevator_rack[index] = None # 나머지는 걍 null
+
+
+
+        # getcall함수로 콜이 발생하는지 계속 체크한다
+
+
+        # simulator ui 를 이런식으로 전달해도 되는지
+        call = self.getcall(ui = self.ui, tn = 1)
+        # 콜이 발생했다고 치고 이게 이제 할당되야 된다
+        self.allocate(call)
+    """
---
+++ newGUI_THREADERROR/readcalls.py
@@ -0,0 +1,251 @@
+# -*- coding: utf-8 -*-
+import threading
+import simulator
+import dataParsing as gdata
+import constant
+import math
+import random
+import time
+import call as c
+
+# http://saelly.tistory.com/510
+class Readcalls(threading.Thread):
+   def __init__(self, parent=None):
+       threading.Thread.__init__(self)
+       self.call_number = 0
+       self.current_floor_term = 20  # 층 차이값일단 최대로 줌
+       self.threadFlag = [0]*4
+
+
+   def settn(self, tn):
+       self.tn = tn
+
+   def setElevator_rack(self, elevator_rack, t1, t2, t3, t4):
+       self.elevator_rack = elevator_rack
+       self.t1 = t1
+       self.t2 = t2
+       self.t3 = t3
+       self.t4 = t4
+
+   def setUI(self, uiUI):
+       self.ui = uiUI
+
+   def getUI(self):
+       return self.ui
+
+   def setDialog(self, uiDialog):
+       self.dialog = uiDialog
+
+   def setcalllist(self, elevator_calls):
+       self.elevator_calls = elevator_calls
+
+   def run(self):
+       self.tn += 1  # 다음 투플로 넘긴다음에 와일문 돌리기
+       while (self.tn < 10000):
+           if self.tn < 10000:
+               # dataSplit = gdata.dataSet[self.tn][0].split('.')
+               registertime = gdata.getData(self.tn)  # gdata.datetime.datetime.strptime(dataSplit[0],"%Y-%m-%d %H:%M:%S")
+
+               if self.dialog.timer[2] == registertime.second and self.dialog.timer[1] == registertime.minute and \
+                               self.dialog.timer[0] == registertime.hour:
+                   call = self.getcall(self.tn)  # 시간 같으면 콜을 발생시킨다.
+                   allocated_elevator = self.allocate3(call)
+                   allocated_elevator = int(allocated_elevator)
+
+                   print("getcall!!!  + allocated elevator num : ", allocated_elevator)
+
+                   # flag가 1로 스탑이라는데 할당된 엘리베이터가 0번 이면(새 콜이 들어왔다는 거) 다시 플래그를 운행중인 0 으로 바꾸어준다
+                   # if( self.threadFlag ==1 and allocated_elevator == 0 ): # 정지상태가 아닌
+                   #    self.threadFlag = 0
+
+                   # 할당받은 엘리베이터의 배열에 넣겠다
+                   self.elevator_rack[allocated_elevator].ready_calls.append(call)
+                   self.tn += 1
+                   print("t1  : ", self.t1.isSet(), "readycall length",len(self.elevator_rack[i].ready_calls))
+
+                    # 1이면 즉시 리턴해 주는 함수 : 기본이 0이므로 기다린다. set - run :  clear - stop
+                   for i in range(0, 4):
+                       # 콜이 없는데 실행중이면 멈추고 스탑으로flag=1바꿈
+                       if (i == 0):
+                           print("EL1 flag : ", self.threadFlag[i], "readycall length",len(self.elevator_rack[i].ready_calls))
+                           if (len(self.elevator_rack[i].ready_calls) == 0 ):
+                               print('EL1 콜이 없고 실행플래그 : 정지시킨다, 정지로 플래그 변경')
+                               self.t1._stop.clear()
+                               #self.threadFlag[i] = 1
+                           elif (len(self.elevator_rack[i].ready_calls) != 0 ):
+                               print('EL1 콜이 있는데 정지플래그 : 쓰레드 실행시킨다, 실행으로 플래그 변경')
+                               self.t1._stop.set()
+                               #self.threadFlag[i] = 0
+
+                       elif (i == 1):
+                           print("EL2 flag : ", self.threadFlag[i], "readycall length",len(self.elevator_rack[i].ready_calls))
+                           if (len(self.elevator_rack[i].ready_calls) == 0 ):
+                               print('EL2 콜이 없고 실행플래그 : 정지시킨다, 정지로 플래그 변경')
+                               self.t2._stop.clear()
+                               #self.threadFlag[i] = 1
+                           elif (len(self.elevator_rack[i].ready_calls) != 0 ):
+                               print('EL2 콜이 있는데 정지플래그 : 쓰레드 실행시킨다, 실행으로 플래그 변경')
+                               self.t2._stop.set()
+                               #self.threadFlag[i] = 0
+
+                       elif (i == 2):
+                           print("EL3 flag : ", self.threadFlag[i], "readycall length",len(self.elevator_rack[i].ready_calls))
+                           if (len(self.elevator_rack[i].ready_calls) == 0 ):
+                               print('EL3 콜이 없고 실행플래그 : 정지시킨다, 정지로 플래그 변경')
+                               self.t3._stop.clear()
+                               #self.threadFlag[i] = 1
+                           elif (len(self.elevator_rack[i].ready_calls) != 0 ):
+                               print('EL3 콜이 있는데 정지플래그 : 쓰레드 실행시킨다, 실행으로 플래그 변경')
+                               self.t3._stop.set()
+                               #self.threadFlag[i] = 0
+                       elif (i == 3):
+                           print("EL4 flag : ", self.threadFlag[i], "readycall length",len(self.elevator_rack[i].ready_calls))
+                           if (len(self.elevator_rack[i].ready_calls) == 0 ):
+                               print('EL4 콜이 없고 ')
+                               self.t4._stop.clear()
+                               #self.threadFlag[i] = 1
+                           elif (len(self.elevator_rack[i].ready_calls) != 0 ):
+                               print('EL4 콜이 있는데 ')
+                               self.t4._stop.set()
+                               #self.threadFlag[i] = 0
+               time.sleep(0.1)
+
+
+               # 가지고 온 시간이 지난 시간의 것이면 pass
+           # self.tn이 100번째가 넘어가면 나간다 와일문
+           else:
+               break
+
+   def getcall(self, tn):  # ui객체를 받아와야 된다
+       # index = 0
+       # 데이터의 시간을 가지고와서 배열에 넣어 놓는다,- 아무튼 시간을 가지고 온다는 뜻
+
+       # tn = 투플넘버를가지고 옵시다 - dataparsing에 접근
+       registertime = gdata.getData(tn)
+
+       departure_floor = gdata.dataSet[tn][2]
+       destination_floor = gdata.dataSet[tn][3]
+       # iserroroccured = gdata.dataSet[tn][13]
+       is_up = gdata.dataSet[tn][4]
+       passenger = 3  # 일단은 세명이라고 했음
+
+       # 데이터 추가
+       dbn_register_time = gdata.dataSet[tn][1]  # 수정
+       day_of_week = gdata.dataSet[tn][5]
+       iserror = gdata.dataSet[tn][6]
+       weekend = gdata.dataSet[tn][7]  # 수정
+       register15min = gdata.dataSet[tn][8]
+
+       # call생성
+       # self, register_time, departure_floor,destination_floor, isup, passenger, dbn_register_time, day_of_week, iserror, weekend, register15min)
+
+       # register_time, departure_floor,destination_floor, isup, passenger, dbn_register_time, day_of_week, iserror, weekend, register15min):
+       new_call = c.Call(registertime, departure_floor, destination_floor, is_up, passenger, dbn_register_time,
+                         day_of_week, iserror, weekend, register15min)  # 1이 up, 0이 down
+
+       # register_time, departure_floor,destination_floor, isup, passenger, dbn_register_time, day_of_week, iserror, weekend, register15min):
+       # new_call = c.Call(registertime, departure_floor, destination_floor, is_up, passenger, dbn_register_time,day_of_week,iserror,weekend, register15min)  # 1이 up, 0이 down
+       self.elevator_calls.append(new_call)
+       print('departure_floor : ', new_call.departure, ' --> ', new_call.destination)
+       self.elevator_calls.append(new_call)  # 나중에 waitingtime 계산할때 쓸라고 배열에 담아놓는다.
+
+       # print( self.elevator_calls[0].departure + ' ::: departure')
+
+       return new_call
+
+   # allocate for one by one
+   def allocate3(self,call):
+       floor_term = 0
+       self.current_floor_term = 20
+
+       self.idle_term =17
+       self.keep = 0
+       close_ele = 0  # 가장 가까운 엘레베이터 번호
+
+       # 각각의 엘리베이터들마다 상황을 비교한다
+       for index in range(0, 4):  # constant.MAX_ELEVATORS
+
+           # 층 차이값일단 최대로 줌
+
+           if (self.elevator_rack[index].state == constant.IDLE_STATE):
+               #일단 후보로 둔다
+               keep = index
+               if (int(self.elevator_rack[index].floor) == call.departure ):
+                   close_ele = index
+                   return close_ele
+               else:
+                   floor_term = abs(int(self.elevator_rack[index].floor) - int(call.departure))
+
+                   if (floor_term < self.idle_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                       self.idle_term = floor_term  # 층 차이를 업데이트한다
+                       close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+                   else:
+                       # 이전에 콜보다 짧
+                       close_ele = keep
+                   return close_ele
+
+           else:  # idle이 아니면
+               if (call.destination < int(self.elevator_rack[index].floor) and (call.isup) == 0):
+                   # 층차이
+                   floor_term = abs(int(self.elevator_rack[index].floor) - int(call.departure))
+
+                   if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                       self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                       close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+
+               if (int(self.elevator_rack[index].floor) < call.destination and (call.isup) == 1):  # 1이 up, 0이 down
+                   # 층 차이
+                   floor_term = abs(int(call.departure) - int(self.elevator_rack[index].floor))
+
+                   if (floor_term < self.current_floor_term):
+                       self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                       close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+               else:
+                   if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                       self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                       close_ele = random.randrange(0, 4)  # 할당하는 엘레베이터 번호를 바꾼다.
+
+       return close_ele
+
+   def allocate2(self, call):
+       # 엘레베이터들의 위치를 가진다
+
+       close_ele = 0  # 가장 가까운 엘레베이터 번호
+       # 층 차이값일단 최대로 줌
+       floor_term = 0
+       self.current_floor_term = 20
+       # 각각의 엘리베이터들마다 상황을 비교한다
+       # for i in range(0,4):
+       for index in range(0, 4):  # constant.MAX_ELEVATORS
+           # while(index < 4):
+           # 같은층인데 idle
+           if (int(self.elevator_rack[index].floor) == call.departure and self.elevator_rack[index].state == constant.IDLE_STATE):
+               print("AAAAA")
+               close_ele = index
+           # 같은층은 아닌데 idle
+           elif (self.elevator_rack[index].state == constant.IDLE_STATE):
+               close_ele = index
+           elif (int(self.elevator_rack[index].floor) < call.destination and (call.isup) == 1):  # 1이 up, 0이 down
+               # 층 차이
+               floor_term = abs(int(call.departure) - int(self.elevator_rack[index].floor))
+
+               if (floor_term < self.current_floor_term):
+                   self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                   close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+           # 내 콜이 내려가는콜이고 , 내위치보다 엘레베이터가 위에 있을 때
+           elif (call.destination < int(self.elevator_rack[index].floor) and (call.isup) == 0):
+               # 층차이
+
+               floor_term = abs(int(self.elevator_rack[index].floor) - int(call.departure))
+
+               if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                   self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                   close_ele = index  # 할당하는 엘레베이터 번호를 바꾼다.
+           else:
+               if (floor_term < self.current_floor_term):  # 제일 짧은 층 차이보다 더 층 차이가 짧을 때
+                   self.current_floor_term = floor_term  # 층 차이를 업데이트한다
+                   close_ele = random.randrange(0, 4)  # 할당하는 엘레베이터 번호를 바꾼다.
+
+       return close_ele
+
+
